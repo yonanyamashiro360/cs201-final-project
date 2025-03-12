7f904020757b55d39bda9a0349d630c24d90277d
yonanyamashiro360 <yonanyamashiro360@gmail.com>
2025-03-12 08:28:36 +0000

    Add 6.rs

---
+++ src/6.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let num = 5;
+    if num < 0 {
+        println!("Negative");
+    } else if num > 10 {
+        println!("Greater than 10");
+    } else {
+        println!("Positive");
+    }
+}
